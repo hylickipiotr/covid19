de5ec2c568f9befc59ffeef14a004946e0e7f80f
hylickipiotr <hylickipiotr@gmail.com>
2020-11-20 21:33:20 +0100

    Fixed bug with favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,16 +7,47 @@ import "../styles/tailwind.css";
 import "../styles/main.css";
 import { SearchProvider } from "../components/Search/SearchContext";
 import { CacheProvider } from "../contexts/Cache";
+import Head from "next/head";
 
 library.add(fas);
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <CacheProvider>
-      <SearchProvider>
-        <Component {...pageProps} />
-      </SearchProvider>
-    </CacheProvider>
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          name="description"
+          content="Śledź statystyki COVID-19 z wszystkich krajów świata."
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icon-16x16.png"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#e53e3e" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <CacheProvider>
+        <SearchProvider>
+          <Component {...pageProps} />
+        </SearchProvider>
+      </CacheProvider>
+    </>
   );
 };
 
