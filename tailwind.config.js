7501247db8e68e18050955ecc885db91b7ae675b
hylickipiotr <hylickipiotr@gmail.com>
2020-11-23 10:16:57 +0100

    Added tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require("tailwindcss/colors");
 const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-  purge: [],
+  purge: ["./src/**/*.jsx", "./src/**/*.tsx"],
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
