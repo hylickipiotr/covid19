beeda7fb5bfbef912f681c6d7324e615b4f7f797
hylickipiotr <hylickipiotr@gmail.com>
2020-11-30 18:15:19 +0100

    Switched custom transition to default tw transition

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -12,7 +12,7 @@ const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = ({
       }}
       className={classNames(
         "input",
-        "bg-white text-gray-800 border border-gray-300 rounded px-2 transition-all duration-200 ease-in-out  disabled:border-gray-300 disabled:bg-gray-200 disabled:text-gray-400 disabled:cursor-not-allowed focus:outline-none focus:ring-1",
+        "bg-white text-gray-800 border border-gray-300 rounded px-2 transition  disabled:border-gray-300 disabled:bg-gray-200 disabled:text-gray-400 disabled:cursor-not-allowed focus:outline-none focus:ring-1",
         className
       )}
       {...props}
