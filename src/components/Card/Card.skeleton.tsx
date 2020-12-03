1ab0cbc1b0464363f4f772c058028bae38d96e3f
hylickipiotr <hylickipiotr@gmail.com>
2020-12-03 13:52:48 +0100

    Changed card skeleton colors

--- src/components/Card/Card.skeleton.tsx
+++ src/components/Card/Card.skeleton.tsx
@@ -4,11 +4,11 @@ import { cardHeight } from "./Card";
 const CardSkeleton: React.FC<{}> = ({}) => {
   return (
     <div
-      className="bg-gray-300 rounded-md animate-pulse p-4"
+      className="bg-gray-200 rounded-md animate-pulse p-4"
       style={{ height: cardHeight }}
     >
-      <div className="bg-gray-200 h-4 w-3/5" />
-      <div className="bg-gray-200 h-7 w-2/5 mt-2"></div>
+      <div className="bg-gray-100 h-4 w-3/5" />
+      <div className="bg-gray-100 h-7 w-2/5 mt-2"></div>
     </div>
   );
 };
